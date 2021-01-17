0189c96a46e29c960d0ea7884dee431451c96c78
michał.drelich <235902@student.pwr.edu.pl>
2021-01-17 01:04:11 +0100

    powitalna aktywnosc

---
+++ app/src/main/java/com/example/multimedia/WindowOnStart.java
@@ -0,0 +1,21 @@
+package com.example.multimedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WindowOnStart extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_window_on_start);
+    }
+
+    public void openKotlin(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        Button button = (Button) findViewById(R.id.button_kotlin);
+        startActivity(intent);
+    }
+}
